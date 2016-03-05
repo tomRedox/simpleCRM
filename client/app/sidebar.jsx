ac4bfdddf3eaad4f9a67c167bcb2a1a3a410a69f
Tom Woodforde <tom.woodforde@redox-software.co.uk>
2016-03-05 13:49:36 +0000

    Top app bar now not scrolling, and sidebar below top bar (cherry picked from commit da2339f)

--- client/app/sidebar.jsx
+++ client/app/sidebar.jsx
@@ -1,50 +1,119 @@
 
-//var TextInput = require('./textInput');
-var React = require('react');
-// import { Link } from 'react-router';
+
+import React, { Component, PropTypes } from 'react';
+
+import LeftNav from 'material-ui/lib/left-nav';
+import AppBar from 'material-ui/lib/app-bar';
+import RaisedButton from 'material-ui/lib/raised-button';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+
+import { toggleLeftNavExpanded } from '../redux/ui-actions.jsx';
+
 
 // App component - represents the whole app
-Sidebar = React.createClass({
+const SidebarContainer = React.createClass({
 
-    render() {
-        // console.log("CustomerEditComponent props: ", this.props);
+    renderExpandedSideBar() {
+
+        const navStyle = {
+            marginTop: 64,
+            height: (window.innerHeight - 64)
+        };
 
         return (
-            <div className="column col-sm-2 col-xs-1 sidebar-offcanvas" id="sidebar">
 
-                <ul className="nav">
-                    <li><a href="#" data-toggle="offcanvas" className="visible-xs text-center">
-                        <i className="fa fa-chevron-right"/></a></li>
-                </ul>
+            <div>
+                <LeftNav width={220} open={true} style={navStyle} className="MY_LeftNav">
+                    <MenuItem>Menu Item</MenuItem>
+                    <MenuItem>Menu Item 2</MenuItem>
 
-                <ul className="nav hidden-xs" id="lg-menu">
-                    <li className="active"><a href="/"><i className="fa fa-tachometer"/> Dashboard</a></li>
-                    <li><a href="/allOrders"><i className="fa fa-file-text"/> Orders</a></li>
-                    <li><a href="/allCustomers"><i className="fa fa-group"/> Customers</a></li>
-                    <li><a href="/products"><i className="fa fa-archive"/> Products</a></li>
-                    <li><a href="/test1"><i className="fa fa-search"/> Search</a></li>
-                    <li><a href="/test2"><i className="fa fa-line-chart"/> Reports</a></li>
-                </ul>
-                <ul className="list-unstyled hidden-xs" id="sidebar-footer">
-                    <li>
-                        <a href="http://www.redox-software.co.uk"><h3>simple crm</h3>
-                            <i className="fa fa-cloud"/> redox software ltd</a>
-                    </li>
-                </ul>
-
-                <ul className="nav visible-xs" id="xs-menu">
-                    <li><a href="/" className="text-center"><i className="fa fa-tachometer"/></a></li>
-                    <li><a href="/allOrders" className="text-center"><i className="fa fa-file-text"/></a></li>
-                    <li><a href="/allCustomers" className="text-center"><i className="fa fa-group"/></a></li>
-                    <li><a href="/products" className="text-center"><i className="fa fa-archive"/></a></li>
-                    <li><a href="#" className="text-center"><i className="fa fa-search"/></a></li>
-                    <li><a href="#" className="text-center"><i className="fa fa-line-chart"/></a></li>
-                </ul>
-
+                </LeftNav>
             </div>
-
         );
+    },
+
+//        <li className="active"><a href="/"><i className="fa fa-tachometer"/> Dashboard</a></li>
+//        <li><a href="/allOrders"><i className="fa fa-file-text"/> Orders</a></li>
+//        <li><a href="/allCustomers"><i className="fa fa-group"/> Customers</a></li>
+//        <li><a href="/products"><i className="fa fa-archive"/> Products</a></li>
+//        <li><a href="/test1"><i className="fa fa-search"/> Search</a></li>
+//        <li><a href="/test2"><i className="fa fa-line-chart"/> Reports</a></li>
+
+
+renderMinimisedSideBar() {
+
+    const navStyle = {
+        marginTop: 64,
+        height: (window.innerHeight - 64)
+    };
+
+    return (
+            <div>
+                <LeftNav width={65} style={navStyle} open={true} >
+
+
+                 </LeftNav>
+            </div>
+        );
+    },
+
+    render()
+    {
+        console.log("SidebarContainer.render() props: ", this.props);
+
+        if (this.props.expanded) {
+            return this.renderExpandedSideBar()
+        } else {
+            return <div></div>
+        }
     }
 });
 
-module.exports = Sidebar;
+SidebarContainer.propTypes = {
+    expanded: PropTypes.bool.isRequired
+};
+
+function mapStateToProps(state) {
+    //console.log("SidebarContainer.mapStateToProps", state)
+    return {
+        expanded: state.userInterface.leftNavExpanded
+    };
+}
+
+export default connect(mapStateToProps, {
+    toggleLeftNavExpanded
+})(SidebarContainer);
+
+
+//<div className="column col-sm-2 col-xs-1 sidebar-offcanvas" id="sidebar">
+//
+//    <ul className="nav">
+//        <li><a href="#" data-toggle="offcanvas" className="visible-xs text-center">
+//            <i className="fa fa-chevron-right"/></a></li>
+//    </ul>
+//
+//    <ul className="nav hidden-xs" id="lg-menu">
+//        <li className="active"><a href="/"><i className="fa fa-tachometer"/> Dashboard</a></li>
+//        <li><a href="/allOrders"><i className="fa fa-file-text"/> Orders</a></li>
+//        <li><a href="/allCustomers"><i className="fa fa-group"/> Customers</a></li>
+//        <li><a href="/products"><i className="fa fa-archive"/> Products</a></li>
+//        <li><a href="/test1"><i className="fa fa-search"/> Search</a></li>
+//        <li><a href="/test2"><i className="fa fa-line-chart"/> Reports</a></li>
+//    </ul>
+//    <ul className="list-unstyled hidden-xs" id="sidebar-footer">
+//        <li>
+//            <a href="http://www.redox-software.co.uk"><h3>simple crm</h3>
+//                <i className="fa fa-cloud"/> redox software ltd</a>
+//        </li>
+//    </ul>
+//
+//    <ul className="nav visible-xs" id="xs-menu">
+//        <li><a href="/" className="text-center"><i className="fa fa-tachometer"/></a></li>
+//        <li><a href="/allOrders" className="text-center"><i className="fa fa-file-text"/></a></li>
+//        <li><a href="/allCustomers" className="text-center"><i className="fa fa-group"/></a></li>
+//        <li><a href="/products" className="text-center"><i className="fa fa-archive"/></a></li>
+//        <li><a href="#" className="text-center"><i className="fa fa-search"/></a></li>
+//        <li><a href="#" className="text-center"><i className="fa fa-line-chart"/></a></li>
+//    </ul>
+//
+//</div>
--- client/app/top-nav.jsx
+++ client/app/top-nav.jsx
@@ -1,67 +1,101 @@
+import React, { Component, PropTypes } from 'react';
 
-var React = require('react');
+import AppBar from 'material-ui/lib/app-bar';
+
 import GlobalSearch from '../search/global-search.jsx';
+import { toggleLeftNavExpanded } from '../redux/ui-actions.jsx';
+
 
 // this page is wrapped by the wrapper
-TopNav = React.createClass({
+const TopNavContainer = React.createClass({
 
     render() {
 
 
+        let divStyle = {
+            position: "fixed",
+            top:0,
+            width:"100%"
+        }
+
         return (
-            <div className="navbar navbar-blue navbar-static-top">
-                <div className="navbar-header">
-                    <button className="navbar-toggle" type="button" data-toggle="collapse"
-                            data-target=".navbar-collapse">
-                        <span className="sr-only">Toggle</span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                    </button>
-                    <a href="/" className="navbar-brand logo">simple crm</a>
-                </div>
-                <nav className="collapse navbar-collapse" role="navigation">
-                    <form className="navbar-form navbar-left">
-                        <div className="input-group input-group-sm" style={{maxWidth: "360px"}}>
-                             <GlobalSearch id="srch-term" />
-                            <div className="input-group-btn">
-                                <button className="btn btn-default" type="submit"><i className="fa fa-search"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                    <ul className="nav navbar-nav">
-                        <li>
-                            <a href="/"><i className="fa fa-home"></i> Home</a>
-                        </li>
-                        <li>
-                            <a href="/addCustomer" role="button" data-toggle="modal"><i className="fa fa-plus"></i> Post </a>
-                        </li>
-                        <li>
-                            <a href="#"><span className="badge">badge</span></a>
-                        </li>
-                        <li>
-                             <AccountsUIWrapper />
-                        </li>
-                    </ul>
-                    <ul className="nav navbar-nav navbar-right">
-                    </ul>
-                </nav>
+            <div class="header">
+                <AppBar
+                    title="simple crm"
+                    iconElementRight={<GlobalSearch id="srch-term" />}
+                    onLeftIconButtonTouchTap={this.props.toggleLeftNavExpanded}
+                    style={divStyle}
+                />
             </div>
         );
     }
 });
 
+
+TopNavContainer.propTypes = {
+    expanded: PropTypes.bool.isRequired
+};
+
+function mapStateToProps(state) {
+    //console.log("TopNavContainer.mapStateToProps", state)
+    return {
+        expanded: state.userInterface.leftNavExpanded
+    };
+}
+
+export default connect(mapStateToProps, {
+    toggleLeftNavExpanded
+})(TopNavContainer);
+
+
+//<div className="navbar navbar-blue navbar-static-top">
+//    <div className="navbar-header">
+//        <button className="navbar-toggle" type="button" data-toggle="collapse"
+//                data-target=".navbar-collapse">
+//            <span className="sr-only">Toggle</span>
+//            <span className="icon-bar"></span>
+//            <span className="icon-bar"></span>
+//            <span className="icon-bar"></span>
+//        </button>
+//        <a href="/" className="navbar-brand logo">simple crm</a>
+//    </div>
+//    <nav className="collapse navbar-collapse" role="navigation">
+//        <form className="navbar-form navbar-left">
+//            <div className="input-group input-group-sm" style={{maxWidth: "360px"}}>
+//                <GlobalSearch id="srch-term" />
+//                <div className="input-group-btn">
+//                    <button className="btn btn-default" type="submit"><i className="fa fa-search"></i>
+//                    </button>
+//                </div>
+//            </div>
+//        </form>
+//        <ul className="nav navbar-nav">
+//            <li>
+//                <a href="/"><i className="fa fa-home"></i> Home</a>
+//            </li>
+//            <li>
+//                <a href="/addCustomer" role="button" data-toggle="modal"><i className="fa fa-plus"></i> Post </a>
+//            </li>
+//            <li>
+//                <a href="#"><span className="badge">badge</span></a>
+//            </li>
+//            <li>
+//                <AccountsUIWrapper />
+//            </li>
+//        </ul>
+//        <ul className="nav navbar-nav navbar-right">
+//        </ul>
+//    </nav>
+//</div>
+
+
 //<input type="text" className="form-control" placeholder="Search" name="srch-term"
 //       id="srch-term"/>
 
 
-
-    //    <li>
-    //        {{#if isInRole 'Administrator'}}
-    //        <a href="#">Admin</a>
-    //        {{/if}}
-    //    </li>
-    //{{> loginButtons}} <!-- here -->
-
-    module.exports = TopNav;
+//    <li>
+//        {{#if isInRole 'Administrator'}}
+//        <a href="#">Admin</a>
+//        {{/if}}
+//    </li>
+//{{> loginButtons}} <!-- here -->
