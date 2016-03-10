e3f433c3c73fe955bcf5f7669d3532c859ded951
Stan Lindsey <contact@stanlindsey.net>
2016-03-10 17:18:50 +0000

    Manual merge recent commits

--- imports/ui/app/Layout.jsx
+++ imports/ui/app/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import { Provider } from 'react-redux';
+import debugOnly from 'meteor/msavin:debugonly';
+
 import DevTools from '../redux/DevTools.jsx';
-import store from '../redux/store.jsx';
-//import injectTapEventPlugin from 'react-tap-event-plugin';
 import GlobalSearch from '../search/GlobalSearch.jsx';
+
+import store from '../redux/store.jsx';
+
 import AccountsButton from '../security/accounts-button.jsx';
 
 
@@ -13,24 +15,9 @@ Meteor.subscribe("SalesRegions.All");
 Meteor.subscribe("Orders.All");
 
 
-//var checkScrollBars = function(){
-//    var b = $('body');
-//    var normalw = 0;
-//    var scrollw = 0;
-//    if(b.prop('scrollHeight')>b.height()){
-//        normalw = window.innerWidth;
-//        scrollw = normalw - b.width();
-//        $('#container').css({marginRight:'-'+scrollw+'px'});
-//    }
-//}
-//
-//checkScrollBars();
-
-//injectTapEventPlugin();
-
 const ContentContainer = React.createClass({
     render() {
-        console.log("ContentContainer.render()", this.props.children);
+        //console.log("ContentContainer.render()", this.props.children);
 
         return (
             <div key="contentContainer">
@@ -50,11 +37,17 @@ const ContentContainer = React.createClass({
 
 });
 
+function renderDevTools() {
+    if (debugOnly) {
+        return <DevTools /> ;
+    }
+}
 
 export const Layout = ({content}) => (
-    <div>
+    <Provider store={store}>
+        <div>
 
-        <div id="wrapper">
+            <div id="wrapper">
 
             {/* Navigation */}
             <nav className="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
@@ -90,54 +83,44 @@ export const Layout = ({content}) => (
                   </ul>
                 {/* /.navbar-header */}
 
+                    {/* This is the sidebar.  It's inside the top nav somehow */}
+                    <div className="navbar-default sidebar" role="navigation">
+                        <div className="sidebar-nav navbar-collapse">
+                            <ul className="nav" id="side-menu">
 
-                {/* This is the sidebar.  It's inside the top nav somehow */}
-                <div className="navbar-default sidebar" role="navigation">
-                    <div className="sidebar-nav navbar-collapse">
-                        <ul className="nav" id="side-menu">
+                                <li className="sidebar-search">
+                                    <GlobalSearch />
+                                </li>
 
-                            <li className="sidebar-search">
-                                <GlobalSearch />
-                            </li>
+                                <li className="active"><a href="/"><i className="fa fa-tachometer"/> Dashboard</a>
+                                </li>
+                                <li><a href="/allOrders"><i className="fa fa-file-text"/> Orders</a></li>
+                                <li><a href="/allCustomers"><i className="fa fa-group"/> Customers</a></li>
+                                <li><a href="/products"><i className="fa fa-archive"/> Products</a></li>
+                                <li><a href="/test1"><i className="fa fa-search"/> Search</a></li>
+                                <li><a href="/test2"><i className="fa fa-line-chart"/> Reports</a></li>
 
-                            <li className="active"><a href="/"><i className="fa fa-tachometer"/> Dashboard</a></li>
-                            <li><a href="/allOrders"><i className="fa fa-file-text"/> Orders</a></li>
-                            <li><a href="/allCustomers"><i className="fa fa-group"/> Customers</a></li>
-                            <li><a href="/products"><i className="fa fa-archive"/> Products</a></li>
-                            <li><a href="/test1"><i className="fa fa-search"/> Search</a></li>
-                            <li><a href="/test2"><i className="fa fa-line-chart"/> Reports</a></li>
-
-                        </ul>
-                    </div>
-                    {/* /.sidebar-collapse */}
-                </div>
-                {/* /.navbar-static-side */}
-            </nav>
-        </div>
-
-
-        <div id="page-wrapper">
-
-            <Provider store={store}>
-                <div>
-
-
-                        <div>
-                            <div id="popup"></div>
-
-                            <ContentContainer key="content">
-                                {content}
-                            </ContentContainer>
-
-                            {/*<DevTools />*/}
-
+                            </ul>
                         </div>
+                        {/* /.sidebar-collapse */}
+                    </div>
+                    {/* /.navbar-static-side */}
+                </nav>
+            </div>
 
-                </div>
-            </Provider>
+            <div id="page-wrapper">
 
+                <div id="popup"></div>
+
+                <ContentContainer key="content">
+                    {content}
+                </ContentContainer>
+
+
+            </div>
+            {/* /#page-wrapper */}
+
+            { renderDevTools() }
         </div>
-        {/* /#page-wrapper */}
-    </div>
-
+    </Provider>
 );
