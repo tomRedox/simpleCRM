3f3e79537c457e2d6e6e56d376850389a6768d6c
Javier Suarez <javiersuarezl@outlook.com>
2016-03-29 22:13:43 -0600

    Update to login
    Added a inline style on login.jsx.

--- imports/ui/security/login.jsx
+++ imports/ui/security/login.jsx
@@ -3,6 +3,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const forgotStyle = {
+  color: 'black',
+  size: 24,
+  textDecoration: 'none' ,
+};
+
 Login = React.createClass({
   onSubmit(event) {
     event.preventDefault();
@@ -19,43 +25,42 @@ Login = React.createClass({
   },
   render() {
     return (
-      <div>
-        <div className="row">
-          <div className="col-md-4 col-md-offset-4">
-              <div className="login-panel panel panel-default">
-                  <div className="panel-heading">
-                      <h3 className="panel-title">Please Sign In</h3>
+      <div className="row">
+        <div className="col-md-4 col-md-offset-4">
+          <div className="login-panel panel panel-default">
+            <div className="panel-heading">
+              <h4 className="text-center">Please Login</h4>
+            </div>
+            <div className="panel-body">
+              <form onSubmit={this.onSubmit}>
+                <fieldset>
+                  <div className="form-group">
+                    <input className="form-control" placeholder="Email Address" ref="emailAddress" name="email" type="email" autofocus></input>
                   </div>
-                  <div className="panel-body">
-                      <form role="form" onSubmit={this.onSubmit}>
-                          <fieldset>
-                              <div className="form-group">
-                                  <input className="form-control"
-                                    placeholder="Email Address"  ref="emailAddress"
-                                    name="email" type="email" autofocus></input>
-                              </div>
-                              <div className="form-group">
-                                  <input className= "form-control" className="form-control" ref="password" placeholder="Password" name="password" type="password" value=""></input>
-                              </div>
-                              {/*<div className="checkbox">
-                                  <label>
-                                      <input name="remember" type="checkbox" value="Remember Me">Remember Me
-                                  </label>
-                              </div>*/}
-                              <div>
-                                <span className="pull-right" href={{ pathFor 'recover-password'}}>Forgot Password?</span>
-                              </div>
-                              <div>
-                                <input type="submit" value="Login" className="btn btn-lg btn-success btn-block">Login</input>
-                              </div>
-                          </fieldset>
-                      </form>
+                  <div className="form-group">
+                    <input className="form-control" className="form-control" ref="password" placeholder="Password" name="password" type="password"></input>
                   </div>
-              </div>
+                  <div className="row">
+                    <div className="col-xs-12 col-sm-6 checkbox">
+                      <label>
+                        <input className="pull-left" name="remember" type="checkbox" value="Remember Me">Remember Me</input>
+                      </label>
+                    </div>
+                    <div className="col-xs-12 col-sm-6">
+                      <a style={forgotStyle} className="pull-right" href="{{pathFor 'recover-password'}}">Forgot Password?</a>
+                    </div>
+                  </div>
+                  <div>
+                    <input type="submit" value="Login" className="btn btn-lg btn-success btn-block"></input>
+                  </div>
+                </fieldset>
+              </form>
+            </div>
           </div>
+        </div>
       </div>
     );
-  },
+  }
 });
 
 export default Login;
