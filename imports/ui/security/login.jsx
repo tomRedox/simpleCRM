c0d47106d1125725c28b0bea4f3bb708447b63ad
Javier Suarez <javiersuarezl@outlook.com>
2016-03-29 20:35:49 -0600

    New login
    New UI for the login page.

--- imports/ui/security/login.jsx
+++ imports/ui/security/login.jsx
@@ -4,42 +4,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 Login = React.createClass({
-    onSubmit(event) {
-      event.preventDefault();
-      let emailAddress = ReactDOM.findDOMNode(this.refs.emailAddress).value.trim();
-      let password = ReactDOM.findDOMNode(this.refs.password).value.trim();
+  onSubmit(event) {
+    event.preventDefault();
+    let emailAddress = ReactDOM.findDOMNode(this.refs.emailAddress).value.trim();
+    let password = ReactDOM.findDOMNode(this.refs.password).value.trim();
 
-      Meteor.loginWithPassword( emailAddress, password, ( error ) => {
-        if ( error ) {
-          console.log(error);
-        } else {
-          FlowRouter.go( 'Home' );
-        }
-      });
-    },
-    render() {
-      return (
-        <div>
-    <h4>Login</h4>
-      <form onSubmit={this.onSubmit}>
-        <div className="form-group">
-          <label for="emailAddress">Email Address</label>
-          <input type="email" ref="emailAddress" className="form-control" placeholder="Email Address" />
-        </div>
-        <div className="form-group">
-          <label for="password">
-            <span className="pull-left">Password</span>
-            <a className="pull-right" href="{{pathFor 'recover-password'}}">Forgot Password?</a>
-          </label>
-          <input type="password" ref="password" className="form-control" placeholder="Password" />
-        </div>
-        <div className="form-group">
-          <input type="submit" className="btn btn-success" value="Login" />
-        </div>
-      </form>
+    Meteor.loginWithPassword(emailAddress, password, (error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        FlowRouter.go('Home');
+      }
+    });
+  },
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-md-4 col-md-offset-4">
+              <div className="login-panel panel panel-default">
+                  <div className="panel-heading">
+                      <h3 className="panel-title">Please Sign In</h3>
+                  </div>
+                  <div className="panel-body">
+                      <form role="form" onSubmit={this.onSubmit}>
+                          <fieldset>
+                              <div className="form-group">
+                                  <input className="form-control"
+                                    placeholder="Email Address"  ref="emailAddress"
+                                    name="email" type="email" autofocus></input>
+                              </div>
+                              <div className="form-group">
+                                  <input className= "form-control" className="form-control" ref="password" placeholder="Password" name="password" type="password" value=""></input>
+                              </div>
+                              {/*<div className="checkbox">
+                                  <label>
+                                      <input name="remember" type="checkbox" value="Remember Me">Remember Me
+                                  </label>
+                              </div>*/}
+                              <div>
+                                <span className="pull-right" href={{ pathFor 'recover-password'}}>Forgot Password?</span>
+                              </div>
+                              <div>
+                                <input type="submit" value="Login" className="btn btn-lg btn-success btn-block">Login</input>
+                              </div>
+                          </fieldset>
+                      </form>
+                  </div>
+              </div>
+          </div>
       </div>
-        );
-     }
+    );
+  },
 });
 
 export default Login;
